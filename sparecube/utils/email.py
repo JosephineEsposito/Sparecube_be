33781a94414744174b4e665733d6a9237abcfb0c
Batoul <batoul.haidar95@gmail.com>
2025-05-09 14:49:42 +0200

    Add Send Email & check for unique ticket/waybill

--- sparecube/utils/email.py
+++ sparecube/utils/email.py
@@ -7,54 +7,120 @@ import os
 # MAILERSEND EMAIL MANAGEMENT
 from mailersend import emails
 
-
-
-def send(oggetto, corpo, email, nome, link = None):
+def send(oggetto, corpo, emails_list, names_list, link=None):
     mailer = emails.NewEmail(os.environ['MAILERSEND_API_KEY'])
 
     mail_body = {}
 
     mail_from = {
-        "name" : os.environ['PROJECT'],
+        "name": os.environ['PROJECT'],
         "email": os.environ['EMAIL_SEND'],
     }
 
-    recipients = [
-        {
-            "name" : nome,
-            "email": email,
-        }
-    ]
+    recipients = []
+    variables = []
 
-    variables = [
-        {
-            "email" : email,
-            "substitutions" : [
-                {
-                    "var" : "name",
-                    "value" : nome
-                },
-                {
-                    "var": "text",
-                    "value": corpo
-                },
-                {
-                    "var" : "link",
-                    "value": link
-                }
-            ]
-        }
-    ]
+    for email, nome in zip(emails_list, names_list):
+        recipients.append({
+            "name": nome,
+            "email": email,
+        })
+
+        variables.append({
+            "email": email,
+            "data": {
+                "name": nome,
+                "text": corpo,
+                "link": link or ""
+            }
+        })
 
     mailer.set_mail_from(mail_from, mail_body)
     mailer.set_mail_to(recipients, mail_body)
     mailer.set_subject(oggetto, mail_body)
     mailer.set_template("3zxk54v71r64jy6v", mail_body)
-    mailer.set_simple_personalization(variables, mail_body)
-    
-    mailer.send(mail_body)
+    mailer.set_personalization(variables, mail_body)
 
-    return {"status" : 0}
+    result = mailer.send(mail_body)
+    print("result", result)
+
+    return {"status": 0}
+
+
+# def send(oggetto, corpo, email, nome, link = None):
+#     mailer = emails.NewEmail(os.environ['MAILERSEND_API_KEY'])
+#     print("email", email)
+#
+#     mail_body = {}
+#
+#     mail_from = {
+#         "name" : os.environ['PROJECT'],
+#         "email": os.environ['EMAIL_SEND'],
+#     }
+#
+#     recipients = [
+#         {
+#             "name" : nome,
+#             "email": email,
+#         }
+#     ]
+#
+#     variables = [
+#         {
+#             "email": email,
+#             "data": {
+#                 "name": nome,
+#                 "text": corpo,
+#                 "link": link or ""
+#             }
+#         }
+#     ]
+#
+#     # variables = [
+#     #     {
+#     #         "email" : email,
+#     #         "substitutions" : [
+#     #             {
+#     #                 "var" : "name",
+#     #                 "value" : nome
+#     #             },
+#     #             {
+#     #                 "var": "text",
+#     #                 "value": corpo
+#     #             },
+#     #             {
+#     #                 "var" : "link",
+#     #                 "value": link
+#     #             }
+#     #         ]
+#     #     }
+#     # ]
+#
+#     # substitutions = [
+#     #     {"var": "name", "value": nome},
+#     #     {"var": "text", "value": corpo},
+#     #     {"var": "link", "value": link or ""}
+#     # ]
+#     #
+#     # personalization = [
+#     #     {
+#     #         "email": email,
+#     #         "substitutions": substitutions
+#     #     }
+#     # ]
+#
+#     mailer.set_mail_from(mail_from, mail_body)
+#     mailer.set_mail_to(recipients, mail_body)
+#     mailer.set_subject(oggetto, mail_body)
+#     mailer.set_template("3zxk54v71r64jy6v", mail_body)
+#     #mailer.set_simple_personalization(variables, mail_body)
+#     mailer.set_personalization(variables, mail_body)
+#
+#     x = mailer.send(mail_body)
+#     print ("result", x)
+#
+#
+#     return {"status" : 0}
 
 def admin_send(oggetto, email, nome, password, link = None):
     
